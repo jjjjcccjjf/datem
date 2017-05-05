3117b9491dafab8221ca85017735ab072c06de8f
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-05-05 16:08:06 +0800

    fix contact us fields

--- wp-content/themes/datem-water/template-contact.php
+++ wp-content/themes/datem-water/template-contact.php
@@ -9,21 +9,36 @@ while(have_posts()): the_post();
     <aside>
       <h4>Address and Directions:</h4>
       <ul>
-        <li class="address">2/F DATEM BUILDING, 99 MINDANAO AVENUE, <br>
-          BRGY. BAHAY TORO, QUEZON CITY, PHILIPPINES 1106</li>
-          <li class="contactno">(632) 923-2836<br>
-            (632) 920-9161</li>
-            <li class="email">info@datem.com.ph <br>
-              jjstabarbara@datem.com.ph</li>
-              <li class="social">/ DATEMInc</li>
-            </ul>
-            <div class="google-maps">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.8340530294467!2d121.03344731523129!3d14.665357179417176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b720255bdc0d%3A0xf0e5933b14004823!2sDATEM+Incorporated!5e0!3m2!1sen!2sph!4v1493799907825" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-          </aside>
-        </article>
-      </section>
-      <div class="contact"></div>
-      <?php
-    endwhile;
-    get_footer();
+        <li class="address"><?php the_field('address') ?></li>
+        <li class="contactno">
+          <?php
+          if( have_rows('phone') ):
+            while ( have_rows('phone') ) : the_row();
+            echo get_sub_field('phone') . "<br>" ;
+          endwhile;
+          else :
+          endif;
+          ?>
+        </li>
+        <li class="email">
+          <?php
+          if( have_rows('email') ):
+            while ( have_rows('email') ) : the_row();
+            echo get_sub_field('email') . "<br>" ;
+          endwhile;
+          else :
+          endif;
+          ?>
+        </li>
+        <li class="social"><?php the_field('social') ?></li>
+      </ul>
+      <div class="google-maps">
+        <?php the_field('google_map'); ?>
+      </div>
+    </aside>
+  </article>
+</section>
+<div class="contact"></div>
+<?php
+endwhile;
+get_footer();
