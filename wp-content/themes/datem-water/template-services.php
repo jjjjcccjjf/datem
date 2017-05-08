3afe4f3fa8d85edf81e524fe55eb3cc57cba7097
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-05-08 11:20:04 +0800

    add services dynamic

--- wp-content/themes/datem-water/template-services.php
+++ wp-content/themes/datem-water/template-services.php
@@ -6,56 +6,52 @@ while(have_posts()): the_post();
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/magnific-popup.css">
 <section class="services-content">
   <article>
-    <h1>Services</h1>
-    <h2>Your partner in providing water and <br>
-      wastewater solution to your community</h2>
-      <ul>
-        <li>
-          <article>
-            <h3 class="gradient">Bulk Water Supply through PPP</h3>
-            <p><a href="index.html" data-mfp-src="#test-popup" class="open-popup-link">>> Read more...</a></p>
-            <aside><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bulkwater.png"></aside>
-          </article>
+    <h1><?php the_title(); ?></h1>
+    <h2><?php the_content() ?></h2>
+    <ul>
+      <?php
 
-        </li>
+      $i = 0; # Counter for our modal
+      $args = array('post_type' => 'service', 'posts_per_page' => -1);
+      $the_query = new WP_Query($args);
+      if ( $the_query->have_posts() ) {  while ( $the_query->have_posts() ): $the_query->the_post(); ?>
         <li>
           <article>
-            <h3 class="gradient">Wastewater Management</h3>
-            <p><a href="index.html" data-mfp-src="#test-popup1" class="open-popup-link">>> Read more...</a></p>
+            <h3 class="gradient"><?php the_title(); ?></h3>
+            <p><a href="#" data-mfp-src="#test-popup<?= $i ?>" class="open-popup-link">>> Read more...</a></p>
           </article>
-          <aside><img src="<?php echo get_template_directory_uri(); ?>/assets/images/wastewater.png"></aside>
+          <aside><img src="<?php the_field('side_logo')?>"></aside>
         </li>
-        <li>
-          <article>
-            <h3 class="gradient">Bulk Water Supply through PPP</h3>
-            <p><a href="#">>> Read more...</a></p>
-          </article>
-          <aside><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bulkwater.png"></aside>
-        </li>
-      </ul>
-      <div id="test-popup" class="white-popup mfp-hide">
-        <div class="contents">
-          <p><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bulkwater-supply.jpg"></p>
-          <h3>Bulk Water Supply through PPP</h3>
-          <p>Engineering, Procurement & Construction + Operation (EPC+O): DATEM Water will construct the required facilities for the client on a progressive billing scheme then DATEM Water will then operate the said facility for the client.</p>
-          <p>Build Own Operate (BOO): DATEM Water will design, finance, build and operate the facility on behalf of the client for an agreed amount.</p>
-          <p>Build Operate Transfer (BOT): Similar with BOO but will be limited by a cooperation period.</p>
-        </div>
-      </div>
-      <div id="test-popup1" class="white-popup mfp-hide">
-        <div class="contents">
-          <p><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bulkwater-supply.jpg"></p>
-          <h3>Bulk Water Supply through PPP 2</h3>
-          <p>Engineering, Procurement & Construction + Operation (EPC+O): DATEM Water will construct the required facilities for the client on a progressive billing scheme then DATEM Water will then operate the said facility for the client.</p>
-          <p>Build Own Operate (BOO): DATEM Water will design, finance, build and operate the facility on behalf of the client for an agreed amount.</p>
-          <p>Build Operate Transfer (BOT): Similar with BOO but will be limited by a cooperation period.</p>
-        </div>
-      </div>
-    </article>
+      <?php $i++; endwhile; wp_reset_postdata();
+    } else {
+      // no posts found
+    } ?>
+  </ul>
 
-  </section>
-  <div class="services"></div>
-  <?php
+  <!-- ######################################### -->
+   <?php
+
+  $i = 0; # Counter for our modal
+  $args = array('post_type' => 'service', 'posts_per_page' => -1);
+  $the_query = new WP_Query($args);
+  if ( $the_query->have_posts() ) {  while ( $the_query->have_posts() ): $the_query->the_post(); ?>
+    <div id="test-popup<?= $i ?>" class="white-popup mfp-hide">
+      <div class="contents">
+        <p><img src="<?php echo get_the_post_thumbnail_url(); ?>"></p>
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_content() ?></p>
+      </div>
+    </div>
+  <?php $i++; endwhile; wp_reset_postdata();
+} else {
+  // no posts found
+} ?>
+
+</article>
+
+</section>
+<div class="services"></div>
+<?php
 endwhile;
 get_footer();
 ?>
@@ -65,14 +61,18 @@ $(document).ready(function() {
 
   $('.open-popup-link').magnificPopup({
     items: [
-      {
-        src: '#test-popup', // CSS selector of an element on page that should be used as a popup
-        type: 'inline'
-      },
-      {
-        src: '#test-popup1', // CSS selector of an element on page that should be used as a popup
-        type: 'inline'
-      }
+      <?php $i = 0; # Counter for our modal
+        $args = array('post_type' => 'service', 'posts_per_page' => -1);
+        $the_query = new WP_Query($args);
+        if ( $the_query->have_posts() ) {  while ( $the_query->have_posts() ): $the_query->the_post(); ?>
+          {
+            src: '#test-popup<?= $i ?>', // CSS selector of an element on page that should be used as a popup
+            type: 'inline'
+          }<?php echo ($i < $the_query->found_posts) ? ",": "";?>
+        <?php $i++; endwhile; wp_reset_postdata();
+      } else {
+        // no posts found
+      } ?>
     ],
     gallery: {
       enabled: true
